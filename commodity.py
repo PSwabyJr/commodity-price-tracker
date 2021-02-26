0c358b391adf975a4dad5d6278db1f5a11418729
PSwabyJr <pswabyjr@gmail.com>
2021-02-26 06:29:27 -0700

    Update commodity.py
    1. Changed "def uploadDictionaryOfPrices(self, filename)" to "def uploadPriceHistory(self, filename)"
    2. Removed "def getPriceHistory(self, dateRange, dateInterval=1):" from this file
    3. Added import json library
    4. Removed priceReposiory on line 17

--- commodity.py
+++ commodity.py
@@ -3,25 +3,25 @@ By: Lorenzo Swaby pswabyjr@gmail.com
 Date Created: 2/22/2021
 Description:
 Commodity class structure developed for tracking the price of
-commodities (gold, silver, stocks, oil, etc.) """
+commodities (gold, silver, stocks, oil, etc.). Pricing data are 
+read from /write to a JSON file."""
 
+import json
 
 class Commodity:
-  
-  TODO: figure out how to read price history stored in a separate file 
-  and how to upload new data to the file. dictionary priceRepository
-  will be responsible for that.
-  
-  priceRepository = {}    #priceRepository is a dictionary
-  
-  def __init__(self, commodityType):
-    self.commodityType = commodityType
+   
+  def __init__(self, filename):
+    self._create(self, filename)
+    self._doesFileStorageExist(self, filename)
+    self.uploadPriceHistory(self, filename)
     
-  def getPriceHistory(self, dateRange, dateInterval = 1):
-  
   def getPriceOnThisDate(self, price, date):
   
   def setPriceOfToday(self, price, todayDate):
   
-  def uploadDictionaryOfPrices(self, filename):
+  def uploadPriceHistory(self, filename):
+  
+  def _doesFileStorageExist(self, filename):
+  #TODO: if filename exists create file
+  
   
