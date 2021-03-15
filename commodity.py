93f7dee5d4eee7360688741c17149ca38a792b89
PSwabyJr <pswabyjr@gmail.com>
2021-03-15 05:15:02 -0700

    Update commodity.py
    Removed JSON components from this file. JSON component will be moved to a separate library file.  A few function name changes, etc.

--- commodity.py
+++ commodity.py
@@ -6,22 +6,17 @@ Commodity class structure developed for tracking the price of
 commodities (gold, silver, stocks, oil, etc.). Pricing data are 
 read from /write to a JSON file."""
 
-import json
-
 class Commodity:
+  
+fileName= none   #file name that stores pricing data  
    
-  def __init__(self, filename):
-    self._create(self, filename)
-    self._doesFileStorageExist(self, filename)
-    self.uploadPriceHistory(self, filename)
-    
-  def getPriceOnThisDate(self, price, date):
-  
-  def setPriceOfToday(self, price, todayDate):
-  
-  def uploadPriceHistory(self, filename):
-  
-  def _doesFileStorageExist(self, filename):
-  #TODO: if filename exists create file
-  
-  
+   def __init__(self):
+      self._doesFileStorageExist(self)
+   def getPriceOnThisDate(self, date):
+      pass  
+   def setPriceOfToday(self):
+      pass  
+   def getPriceHistory(self):   #get full price history recorded on file 
+      pass  
+   def _doesFileStorageExist(self):
+      pass  
